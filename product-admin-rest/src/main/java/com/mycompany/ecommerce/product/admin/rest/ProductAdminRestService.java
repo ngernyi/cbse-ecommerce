36bc51a5b2b666b69de774757599572e74bf9a18
jiaaahui <134369596+jiaaahui@users.noreply.github.com>
2026-01-23 14:53:23 +0800

    final update

--- product-admin-rest/src/main/java/com/mycompany/ecommerce/product/admin/rest/ProductAdminRestService.java
+++ product-admin-rest/src/main/java/com/mycompany/ecommerce/product/admin/rest/ProductAdminRestService.java
@@ -7,7 +7,7 @@ import com.mycompany.ecommerce.api.model.Product;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/admin")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProductAdminRestService {
